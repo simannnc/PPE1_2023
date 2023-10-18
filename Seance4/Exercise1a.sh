bf09e9d208203f868365a4b21265aa0c95d8bc98
siman <siman@wifi-16-12-211.wifi.univ-paris3.fr>
2023-10-18 12:18:02 +0200

    seance4

---
+++ Seance4/Exercise1a.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+YEAR=$1
+ENTITY=$2
+
+NUMBER_ENTITY=$(cat /User/siman/PPE1_2023/Files/ann/$YEAR/*/* |egrep $ENTITY |wc -l)
+echo "Number of annotations of $ENTITY for $YEAR: $NUMBER_ENTITY"
